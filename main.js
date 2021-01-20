9b012c0bcf94d92626b8a1415c50bcb42f965b1b
wickeddestiny <michajlo.rakonjac@gmail.com>
2021-01-20 03:05:18 +0100

    Fixes

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+//          Modal Shows
+
 $("#configBtn").click(function(){
     if($("#configBtn")){
         $(".modal-settings").css("display","flex")
@@ -8,3 +11,59 @@ $("#closeBtn").click(function(){
         $(".modal-settings").css("display","none")
     }
 });
+
+
+
+
+
+
+//          Timer
+
+
+
+const startingMinutes = 10;
+let time = startingMinutes * 60;
+let playBtn = $("#playBtn");
+let pauseBtn = $("#pauseBtn");
+
+// Clock Path
+const countdownEl = document.getElementById("clock");
+
+
+
+// Countdown
+function updateCountDown() {
+    
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    // Time below 0
+    time = time < 0 ? 0 : time; 
+
+    // Removing additional 0
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    // Displaying
+    countdownEl.innerHTML = `${minutes} : ${seconds}`;
+    time--
+    setInterval(updateCountDown , 1000)
+
+    
+}
+$("#btnStart").click(function(){
+    if($("#btnStart")){
+        updateCountDown()
+        $("#playBtn").removeClass();
+        $("#playBtn").addClass("fas fa-pause");
+    }else{
+
+    }
+});
+$("#btnStop").click(function(){
+    if($("#btnStop")){
+        time=0;
+        $("#playBtn").removeClass();
+        $("#playBtn").addClass("fas fa-play");
+    }
+});
+
