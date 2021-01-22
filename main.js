25e6c1b88ee1a1b36adccf39f77f5a82fa499146
wickeddestiny <michajlo.rakonjac@gmail.com>
2021-01-22 15:33:22 +0100

    Fixes 22.1

--- main.js
+++ main.js
@@ -1,4 +1,108 @@
 
+
+
+
+//              Timer
+
+
+//             *Legend*
+let startBtn = document.getElementById("btnStart");
+let stopBtn = document.getElementById("btnStop");
+
+let focusMin = document.getElementById("focusMinutes");
+let focusSec = document.getElementById("focusSeconds");
+
+let finishSound = new Audio('Mp3-alarm-clock.mp3')
+
+
+
+
+
+// Setting **
+let focusSettingM = document.getElementById("focusSettingMinutes");
+let focusSettingS = document.getElementById("focusSettingSeconds");
+
+let smallSettingM = document.getElementById("smallSettingMinutes");
+let SmallSettingS = document.getElementById("smallSettingSeconds");
+
+let LongSettingM = document.getElementById("longSettingMinutes");
+let LongSettingS = document.getElementById("longSettingSeconds");
+
+
+var counting;
+
+
+
+
+$("#btnStart").click(function(){
+    if(counting === undefined){
+        counting = setInterval(focusTimer,1000);
+        $("#playBtn").removeClass();
+        $("#playBtn").addClass("fas fa-pause");
+        $("#focusStance").text("Playing");
+        
+    }
+    else if ($("#playBtn").hasClass("fas fa-pause")){
+        clearInterval(counting);
+        $("#playBtn").addClass("fas fa-play");
+        counting=undefined;
+        $("#focusStance").text("Paused");
+    }
+});
+
+$("#btnStop").click(function(){
+    if(counting || counting == undefined){
+        clearInterval(counting);
+        focusMin.innerText = 20;
+        focusSec.innerText = '00';
+        $("#playBtn").addClass("fas fa-play");
+        $("#focusStance").text("Paused");
+        
+    }
+});
+
+
+
+function focusTimer(){
+    if(focusSec.innerText != 0){
+        focusSec.innerText--;
+
+    } else if (focusMin.innerText != 0 && focusSec.innerText == 0){
+        
+        focusSec.innerText = 59;
+        focusMin.innerText--;
+    } else if ( focusMin.innerText == 0 && focusSec.innerText == 0){
+        finishSound.play()
+    }
+
+}
+
+
+
+
+window.onload=function(){
+    $("#focusBtn").click();
+    }
+
+
+$("#focusBtn").click(() =>{
+    $("#focusBtn").css("background-color","rgb(255, 100, 110)")
+    $("#smallBreakBtn").css("background-color","rgb(255, 180, 185)")
+    $("#longBreakBtn").css("background-color","rgb(255, 180, 185)")
+});
+$("#smallBreakBtn").click(() =>{
+    $("#smallBreakBtn").css("background-color","rgb(255, 100, 110)")
+    $("#focusBtn").css("background-color","rgb(255, 180, 185)")
+    $("#longBreakBtn").css("background-color","rgb(255, 180, 185)")
+});
+$("#longBreakBtn").click(() =>{
+    $("#longBreakBtn").css("background-color","rgb(255, 100, 110)")
+    $("#focusBtn").css("background-color","rgb(255, 180, 185)")
+    $("#smallBreakBtn").css("background-color","rgb(255, 180, 185)")
+});
+
+
+
 //          Modal Shows
 
 $("#configBtn").click(function(){
@@ -17,11 +121,25 @@ $("#closeBtn").click(function(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //          Timer
 
 
-
-const startingMinutes = 10;
+/*
+const startingMinutes = 2;
 let time = startingMinutes * 60;
 let playBtn = $("#playBtn");
 let pauseBtn = $("#pauseBtn");
@@ -45,25 +163,33 @@ function updateCountDown() {
 
     // Displaying
     countdownEl.innerHTML = `${minutes} : ${seconds}`;
-    time--
-    setInterval(updateCountDown , 1000)
+
+    time--;
+    if (time < 0){
+        console.log("1")
+    }
+    
 
     
 }
+
+
+
+
 $("#btnStart").click(function(){
-    if($("#btnStart")){
-        updateCountDown()
+    if($("#btnStart").focus()){
+        setInterval(updateCountDown , 1000)
         $("#playBtn").removeClass();
         $("#playBtn").addClass("fas fa-pause");
     }else{
-
+        
     }
 });
 $("#btnStop").click(function(){
     if($("#btnStop")){
-        time=0;
+        time = 0;
         $("#playBtn").removeClass();
         $("#playBtn").addClass("fas fa-play");
     }
 });
-
+*/
